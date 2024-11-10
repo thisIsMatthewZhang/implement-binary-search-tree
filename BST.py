5051243c458d2ae8438e623572d33111fd1a1567
Matthew Zhang <zhangmatt22@gmail.com>
2024-11-10 13:56:41 -0600

    included methods to be implemented for BST and Node class

---
+++ BST.py
@@ -0,0 +1,23 @@
+import Node
+
+class BST:
+    def __init__(self, root: Node):
+        self.root = root
+
+    def put(self, val: int):
+        pass
+
+    def get(self, val: int):
+        pass
+
+    def contains(self, val: int):
+        pass
+
+    def remove(self, val: int):
+        pass
+
+    def height(self):
+        pass
+
+    def size(self):
+        pass
