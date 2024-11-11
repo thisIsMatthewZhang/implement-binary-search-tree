5839e02657aa8bb13bf8d3bda43722cb6b0c88fc
Matthew Zhang <zhangmatt22@gmail.com>
2024-11-11 00:25:57 -0600

    added classes to test each BST.py method

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
 from unittest import TestCase
+
+class TestPut(TestCase):
+    pass
+
+class TestGet(TestCase):
+    pass
+
+class TestContains(TestCase):
+    pass
+
+class TestRemove(TestCase):
+    pass
+
+class TestHeight(TestCase):
+    pass
+
+class TestSize(TestCase):
+    pass
+
