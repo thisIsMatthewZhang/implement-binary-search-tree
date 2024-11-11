7ecd12b5548606e81c13dc3c67611856b9bf886f
Matthew Zhang <zhangmatt22@gmail.com>
2024-11-11 11:07:53 -0600

    added test methods in TestPut class

--- test.py
+++ test.py
@@ -1,7 +1,17 @@
 from unittest import TestCase
+from BST import BST
 
 class TestPut(TestCase):
-    pass
+    def test_that_bst_is_none_upon_initialization(self):
+        self.bst = BST()
+        self.assertIsNone(self.bst)
+        self.assertEqual(self.bst, None)
+        self.assertEqual(self.bst.root, None)
+
+    def test_putting_node_into_empty_bst(self):
+        self.bst = BST()
+        self.bst.put(10)
+        self.assertIsNotNone(self.bst)
 
 class TestGet(TestCase):
     pass
